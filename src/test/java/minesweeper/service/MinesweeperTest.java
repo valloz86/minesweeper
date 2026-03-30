7d0b685b943809f525d15191417914a6306843c6
mvideo <vallozmeister@gmail.com>
2026-03-30 14:25:01 +0300

    покрыл тестами

--- src/test/java/minesweeper/service/MinesweeperTest.java
+++ src/test/java/minesweeper/service/MinesweeperTest.java
@@ -3,22 +3,19 @@ package minesweeper.service;
 import lombok.extern.slf4j.Slf4j;
 import minesweeper.domain.*;
 
+import minesweeper.exception.MinesweeperRuntimeException;
 import org.assertj.core.util.Throwables;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 
 import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.UUID;
 
-import static minesweeper.service.MinesweeperService.HIDDEN;
-import static minesweeper.service.MinesweeperService.MINE;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static minesweeper.service.MinesweeperService.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -27,20 +24,18 @@ public class MinesweeperTest {
     @Autowired
     MinesweeperService minesweeperService;
 
-
+    /**
+    * Сценарий победы
+    * */
     @Test
-    public void mainTest(){
+    public void winTest(){
         try {
             //получаем мапу с играми
-            Field mapField = MinesweeperService.class.getDeclaredField("minesweeperGameMap");
-            mapField.setAccessible(true);
-            Map<UUID, MinesweeperGame> minesweeperGameMap =
-                    (Map<UUID, MinesweeperGame>) mapField.get(minesweeperService);
+            Map<UUID, MinesweeperGame> minesweeperGameMap = getMinesweeperGameMap();
 
-            //начинаем игру
+            //начинаем игру и получаем все данные о ней
             NewGameRequest newGameRequest = new NewGameRequest(10, 10, 10);
 
-            //получаем все данные о ней
             GameInfoResponse response = minesweeperService.newGame(newGameRequest);
 
             UUID gameId = response.getGameId();
@@ -51,7 +46,6 @@ public class MinesweeperTest {
             for(int i = 0; i < openField.length; i++){
                 for(int j = 0; j < openField[i].length; j++){
                     if(HIDDEN.equals(hiddenField[i][j]) && !MINE.equals(openField[i][j])){
-                        log.info("i = " + i + ", j =" + j + ", hiddenField=" + hiddenField[i][j] + ", openField=" + openField[i][j]);
                         response =  minesweeperService.turnGame(new GameTurnRequest(gameId, j, i));
                     }
                 }
@@ -59,12 +53,190 @@ public class MinesweeperTest {
 
             //проверяем, что игра окончена
             assertTrue(response.isCompleted());
+
+            //проверяем, что мы не наступили на мины и победили
+            int minesCount = 0;
+            for(int i = 0; i < openField.length; i++) {
+                for (int j = 0; j < openField[i].length; j++) {
+                    if(FAIL.equals(openField[i][j])){
+                        fail();
+                    } else if(MINE.equals(openField[i][j])){
+                        minesCount++;
+                    }
+                }
+            }
+
+            //проверяем, что кол-во мин сошлось
+            assertEquals(10, minesCount);
+
+            //проверяем, что игра удалилась
+            minesweeperService.turnGame(new GameTurnRequest(gameId, 0, 0));
+            fail();
+        }  catch (MinesweeperRuntimeException e){
+            assertEquals(GAME_OVER, e.getMessage());
         } catch (Exception e) {
             log.error(Throwables.getStackTrace(e));
             fail();
         }
+    }
 
+    private Map<UUID, MinesweeperGame> getMinesweeperGameMap() throws NoSuchFieldException, IllegalAccessException {
+        Field mapField = MinesweeperService.class.getDeclaredField("minesweeperGameMap");
+        mapField.setAccessible(true);
+        Map<UUID, MinesweeperGame> minesweeperGameMap =
+                (Map<UUID, MinesweeperGame>) mapField.get(minesweeperService);
+        return minesweeperGameMap;
+    }
 
+    /**
+     * Сценарий поражения
+     * */
+    @Test
+    public void failTest(){
+        try {
+            //получаем мапу с играми
+            Map<UUID, MinesweeperGame> minesweeperGameMap = getMinesweeperGameMap();
 
+            //начинаем игру и получаем все данные о ней
+            NewGameRequest newGameRequest = new NewGameRequest(10, 10, 10);
+
+            GameInfoResponse response = minesweeperService.newGame(newGameRequest);
+
+            UUID gameId = response.getGameId();
+            String[][] openField = minesweeperGameMap.get(gameId).getOpenField();
+
+            //ищем и подрываем мину
+            for(int i = 0; i < openField.length; i++){
+                for(int j = 0; j < openField[i].length; j++){
+                    if(MINE.equals(openField[i][j])){
+                        response =  minesweeperService.turnGame(new GameTurnRequest(gameId, j, i));
+                        break;
+                    }
+                }
+            }
+
+            //проверяем, что игра окончена
+            assertTrue(response.isCompleted());
+
+            //проверяем, что мы наступили на мины и проиграли
+            int minesCount = 0;
+            for(int i = 0; i < openField.length; i++) {
+                for (int j = 0; j < openField[i].length; j++) {
+                    if(MINE.equals(openField[i][j])){
+                        fail();
+                    } else if(FAIL.equals(openField[i][j])){
+                        minesCount++;
+                    }
+                }
+            }
+
+            //проверяем, что кол-во мин сошлось
+            assertEquals(10, minesCount);
+
+            //проверяем, что игра удалилась
+            minesweeperService.turnGame(new GameTurnRequest(gameId, 0, 0));
+            fail();
+        } catch (MinesweeperRuntimeException e){
+            assertEquals(GAME_OVER, e.getMessage());
+        } catch (Exception e) {
+            log.error(Throwables.getStackTrace(e));
+            fail();
+        }
+    }
+
+    /**
+     * Сценарий хода в несуществующей игре
+     * */
+    @Test
+    public void noGameTest(){
+        try {
+            minesweeperService.turnGame(new GameTurnRequest(UUID.randomUUID(), 0, 0));
+
+            fail();
+        } catch (MinesweeperRuntimeException e){
+            //проверяем, что случилась нужная ошибка с нужным сообщением
+            assertEquals(GAME_OVER, e.getMessage());
+        } catch (Exception e){
+            log.error(Throwables.getStackTrace(e));
+            fail();
+        }
+    }
+
+    /**
+     * Сценарий повторного открытия клетки
+     * */
+    @Test
+    public void repeatTest(){
+        try {
+            //получаем мапу с играми
+            Map<UUID, MinesweeperGame> minesweeperGameMap = getMinesweeperGameMap();
+
+            //начинаем игру и получаем все данные о ней
+            NewGameRequest newGameRequest = new NewGameRequest(10, 10, 10);
+
+            GameInfoResponse response = minesweeperService.newGame(newGameRequest);
+
+            UUID gameId = response.getGameId();
+            String[][] openField = minesweeperGameMap.get(gameId).getOpenField();
+
+            for(int i = 0; i < openField.length; i++){
+                for(int j = 0; j < openField[i].length; j++){
+                    if(!MINE.equals(openField[i][j])){
+                        minesweeperService.turnGame(new GameTurnRequest(gameId, j, i));
+                        minesweeperService.turnGame(new GameTurnRequest(gameId, j, i));
+                        fail();
+                    }
+                }
+            }
+        } catch (MinesweeperRuntimeException e){
+            //проверяем, что случилась нужная ошибка с нужным сообщением
+            assertEquals(REPEAT, e.getMessage());
+        } catch (Exception e){
+            log.error(Throwables.getStackTrace(e));
+            fail();
+        }
+    }
+
+    @Test
+    public void invalidTest() {
+        try {
+            minesweeperService.newGame(new NewGameRequest(MIN - 1, 10, 1));
+            fail();
+        } catch (MinesweeperRuntimeException e){
+            //проверяем, что случилась нужная ошибка с нужным сообщением
+            assertEquals(INCORRECT_DATA, e.getMessage());
+        }
+
+        try {
+            minesweeperService.newGame(new NewGameRequest(MAX + 1, 10, 1));
+            fail();
+        } catch (MinesweeperRuntimeException e){
+            //проверяем, что случилась нужная ошибка с нужным сообщением
+            assertEquals(INCORRECT_DATA, e.getMessage());
+        }
+
+        try {
+            minesweeperService.newGame(new NewGameRequest(10, MIN - 1, 1));
+            fail();
+        } catch (MinesweeperRuntimeException e){
+            //проверяем, что случилась нужная ошибка с нужным сообщением
+            assertEquals(INCORRECT_DATA, e.getMessage());
+        }
+
+        try {
+            minesweeperService.newGame(new NewGameRequest(10, MAX + 1, 1));
+            fail();
+        } catch (MinesweeperRuntimeException e){
+            //проверяем, что случилась нужная ошибка с нужным сообщением
+            assertEquals(INCORRECT_DATA, e.getMessage());
+        }
+
+        try {
+            minesweeperService.newGame(new NewGameRequest(10, 10, 100));
+            fail();
+        } catch (MinesweeperRuntimeException e){
+            //проверяем, что случилась нужная ошибка с нужным сообщением
+            assertEquals(INCORRECT_DATA, e.getMessage());
+        }
     }
 }
